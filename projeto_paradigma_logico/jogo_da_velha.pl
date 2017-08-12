f67dffb569f67a04ae430022488ed9b74fc12a95
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-08-12 01:36:08 -0300

    Base implementada com sucesso!

--- projeto_paradigma_logico/jogo_da_velha.pl
+++ projeto_paradigma_logico/jogo_da_velha.pl
@@ -35,24 +35,61 @@ tabuleiroVazio([['-', '-', '-'],
 				['-', '-', '-']]).
 
 /* --------------------------------------- list --------------------------------------- */
+% http://www.swi-prolog.org/pldoc/doc_for?object=append/3
 
-replace(L, P, E, R) :- 
-	P1 is (P - 1), 
-	findall(X, (nth0(I,L,Y), (I == P1 -> X = E ; X = Y)), R).
+replace(List, Position, Element, Result) :- 
+	findall(X, (nth1(I,List,Y), (I == Position -> X = Element ; X = Y)), Result).
+
+columnN([],_,[]).
+columnN([H|T], I, [R|X]) :- 
+	nth1(I, H, R), 
+	columnN(T,I,X).
+
+colunas(4, _, _).
+colunas(Index, Tabuleiro, Result) :- 
+	columnN(Tabuleiro, Index, Col),
+	nth1(Index, Result, Col, _),
+	Pos is Index + 1,
+	colunas(Pos, Tabuleiro, Result).
+
+colunas(Tabuleiro, Result) :- 
+	colunas(1, Tabuleiro, Temp),
+	append(Temp, [], Result).
+
+principal([], _,[]).
+principal([H|T], I, [R|X]) :- 
+	nth1(I, H, R),
+	P is I + 1, 
+	principal(T, P, X).
+
+secundaria([], _, []).
+secundaria([H|T], I, [R|X]) :- 
+	nth1(I, H, R),
+	P is I - 1, 
+	secundaria(T, P, X).
+
+diagonais(Tabuleiro, Result) :- 
+	principal(Tabuleiro, 1, Prin),
+	secundaria(Tabuleiro, 3, Sec),
+	nth1(1, Temp, Prin, []),
+	nth1(2, Result, Sec, Temp).
+
+%	Insert Element in Position of List:
+%	nth1(Pos, Result, Element, List).
 
 /* --------------------------------------- list --------------------------------------- */
 
 /* --------------------------------------- mostrar tabuleiro e menu --------------------------------------- */
 
 imprimeLinha([]).
-imprimeLinha([A|B]) :- write(A), write('   '), imprimeLinha(B).
+imprimeLinha([H|T]) :- write(H), write('   '), imprimeLinha(T).
 
 imprimeTabuleiro(_, []).
-imprimeTabuleiro(N, [A|B]) :- 
+imprimeTabuleiro(N, [H|T]) :- 
 	write(N), write('   '),
-	imprimeLinha(A), writeln(''), 
+	imprimeLinha(H), writeln(''), 
 	N1 is N + 1, 
-	imprimeTabuleiro(N1, B).
+	imprimeTabuleiro(N1, T).
 
 imprimeMenu :- 
 	writeln("\nmenu 1. Ganhar: se voce tem duas pecas numa linha, ponha a terceira."),
@@ -74,9 +111,10 @@ imprimeJogoDaVelha(T) :-
 oponente(Jogador, Oponente) :- Jogador = 'X', Oponente = 'O'.
 oponente(Jogador, Oponente) :- Jogador = 'O', Oponente = 'X'.
 
-situacao('X', '1').
-situacao('O', '2').
+situacao('X', 'X').
+situacao('O', 'O').
 situacao('V', 'V').
+situacao(_, "RunBarryRun").
 
 processaEntrada("menu 1", Jogador, Tabuleiro, TabAtualizado) :- 
 	writeln("Menu 1"),
@@ -98,18 +136,26 @@ processaEntrada("menu 5", Jogador, Tabuleiro, TabAtualizado) :-
 	writeln("Menu 5"),
 	TabAtualizado = Tabuleiro.
 
-processaEntrada([Linha,Coluna|[]], Jogador, Tabuleiro, TabAtualizado) :- 
+espacoOcupado(Linha, Coluna, Jogador, Tabuleiro, TabAtualizado) :- 
+	nth1(Linha, Tabuleiro, Lista),
+	nth1(Coluna, Lista, Elemento),
+	Elemento \= '-',
+	writeln("    Espaco Ocupado!"),
+	processaEntrada(Jogador, Tabuleiro, TabAtualizado).
+
+processaEntrada([Linha, Coluna|[]], Jogador, Tabuleiro, TabAtualizado) :- 
+	espacoOcupado(Linha, Coluna, Jogador, Tabuleiro, TabAtualizado);
 	nth1(Linha, Tabuleiro, Lista),
 	replace(Lista, Coluna, Jogador, NovaLinha),
 	replace(Tabuleiro, Linha, NovaLinha, TabAtualizado).
 
 processaEntrada(_, Jogador, Tabuleiro, TabAtualizado) :- 
-	writeln("   INVÁLIDO! Padrão: [lin,col]. ou [menu X]."),
+	writeln("   INVÁLIDO! Padrão: [lin, col]. ou [menu X]."),
 	writeln("      [1, 1].\n      [2, 3].\n      \"menu 5\"."),
 	processaEntrada(Jogador, Tabuleiro, TabAtualizado).
 
 processaEntrada(Jogador, Tabuleiro, TabAtualizado) :- 
-	write("Joga "), write(Jogador), write(" [lin col] ou [menu X]: "),
+	write("Joga "), write(Jogador), write(" [lin,col]. ou \"menu X\".: "),
 	read(Entrada),
 	processaEntrada(Entrada, Jogador, Tabuleiro, TabAtualizado).
 
@@ -117,32 +163,52 @@ jogadorJoga(Tabuleiro, Jogador, TabAtualizado) :-
 	imprimeMenu,
 	processaEntrada(Jogador, Tabuleiro, TabAtualizado).
 
-% TODO: Como fazer para VERIFICAR a vitória???
-verificaVitoria(Tabuleiro, Jogador, Resultado) :-
-%	string_concat("Parabens, jogador", Jogador, Parcial), string_concat(Parcial, "! Voce venceu!", Resultado),
-%	verificaLinhas(Tabuleiro, Jogador, ResultLinhas),
-%	verificaColunas(Tabuleiro, Jogador, ResultColunas),
-	Resultado = 'X',
-	writeln(Resultado).
+direcoes(Tabuleiro, Direcoes) :- 
+	colunas(Tabuleiro, Colunas),
+	append(Tabuleiro, Colunas, Temp),
+	diagonais(Tabuleiro, Diagonais),
+	append(Temp, Diagonais, Direcoes).
 
-turnoJogador(_, _, '1', "Parabens, jogador X! Voce venceu!").
-turnoJogador(_, _, '2', "Parabens, jogador O! Voce venceu!").
-turnoJogador(_, _, 'V', "Deu velha!").
+empate([], 'V').
+empate([[C1,C2,C3]|T], Result) :- 
+	C1 \= '-', C2 \= '-', C3 \= '-', empate(T, Result).
+
+vitoria(C1, C2, C3, Jogador, Result) :- 
+	C1 == Jogador, C2 == Jogador, C3 == Jogador, Result = Jogador.
+
+verificaVitoria([], _, _, false).
+verificaVitoria(_, _, 'X', 'X').
+verificaVitoria(_, _, 'O', 'O').
+verificaVitoria([[C1,C2,C3]|T], Jogador, Situacao, Result) :- 
+	vitoria(C1, C2, C3, Jogador, Temp), Result = Temp;
+	verificaVitoria(T, Jogador, "RunBarryRun", Result).
+
+verificaVitoria(Tabuleiro, Jogador, Result) :- 
+	empate(Tabuleiro, Result);
+	verificaVitoria(Tabuleiro, Jogador, "RunBarryRun", Result).
+
+turnoJogador(Tabuleiro, _, 'X', "Parabens, jogador X! Voce venceu!") :- 
+	imprimeJogoDaVelha(Tabuleiro).
+turnoJogador(Tabuleiro, _, 'O', "Parabens, jogador O! Voce venceu!") :- 
+	imprimeJogoDaVelha(Tabuleiro). 
+turnoJogador(Tabuleiro, _, 'V', "Deu velha!") :- 
+	imprimeJogoDaVelha(Tabuleiro).
 
 turnoJogador(Tabuleiro, Jogador, _, Resultado) :- 
 	imprimeJogoDaVelha(Tabuleiro),
 	jogadorJoga(Tabuleiro, Jogador, TabAtualizado),
-	verificaVitoria(TabAtualizado, Jogador, Temp),
+	direcoes(TabAtualizado, Direcoes),
+	verificaVitoria(Direcoes, Jogador, Temp),
 	situacao(Temp, Situacao),
 	oponente(Jogador, Oponente),
 	turnoJogador(TabAtualizado, Oponente, Situacao, Resultado).
 
 /* --------------------------------------- jogador --------------------------------------- */
 
-:- initialization(main).
+ :- initialization(main).
 
 main :-
-  tabuleiroVazio(T),
-  turnoJogador(T, 'X', "RunBarry", R),
-  writeln(R),
+  tabuleiroVazio(Tabuleiro),
+  turnoJogador(Tabuleiro, 'X', "RunBarryRun", Resultado),
+  writeln(Resultado),
   halt(0).
